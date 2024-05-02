65eb5fe507c3d8a6bdf3987a065b1bb93809b909
yeimmiscamacho <ylcamacho@unicesar.edu.co>
2024-05-02 17:13:18 -0500

    Se agrego la clase

---
+++ ENTITY/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY
+{
+   public class Cliente
+    {
+        public string NumeroDocumento { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string TipoDocumento { get; set; }
+        public string NombreNegocio { get; set; }
+        public string Telefono { get; set; }
+        public string DireccionNegocio { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Contraseña { get; set; }
+    }
+}
