dab4e969ab9f23b89c280191124a7c90a37629a0
Lr-Class <visualitojoselito@gmail.com>
2024-05-03 08:53:21 -0500

    uno dos tres

--- DLL/obj/Debug/net8.0/DLL.AssemblyInfo.cs
+++ DLL/obj/Debug/net8.0/DLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30648ac6c557bfef2d9938c4acd387c328c4e96c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+617e14bd71e88c4556c5e9d03ffb19108fdc3749")]
 [assembly: System.Reflection.AssemblyProductAttribute("DLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ENTITY/obj/Debug/net8.0/ENTITY.AssemblyInfo.cs
+++ ENTITY/obj/Debug/net8.0/ENTITY.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ENTITY")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30648ac6c557bfef2d9938c4acd387c328c4e96c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+617e14bd71e88c4556c5e9d03ffb19108fdc3749")]
 [assembly: System.Reflection.AssemblyProductAttribute("ENTITY")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ENTITY")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
