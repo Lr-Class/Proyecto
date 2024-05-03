71c03e138e2673493ecd4eda80c6d12240873d53
Lr-Class <visualitojoselito@gmail.com>
2024-05-03 08:39:21 -0500

    Cambios de error

--- BLL/obj/Debug/net8.0/BLL.AssemblyInfo.cs
+++ BLL/obj/Debug/net8.0/BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30648ac6c557bfef2d9938c4acd387c328c4e96c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8085892b042214a56b01b5e70e34220b5833fb11")]
 [assembly: System.Reflection.AssemblyProductAttribute("BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
