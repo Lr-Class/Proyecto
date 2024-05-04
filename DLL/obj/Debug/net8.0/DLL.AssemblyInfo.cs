24881b8a39719c5b6b1cba2e7fb0a154b3893309
Lr-Class <visualitojoselito@gmail.com>
2024-05-04 15:01:46 -0500

    as

--- DLL/obj/Debug/net8.0/DLL.AssemblyInfo.cs
+++ DLL/obj/Debug/net8.0/DLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71c03e138e2673493ecd4eda80c6d12240873d53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e454392e3a77e3f16b4b41d5c2b5cde2a0fd3a0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
