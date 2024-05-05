71e502ed4821aa5cba26b6a4d458dd5e8041afba
yeimmiscamacho <ylcamacho@unicesar.edu.co>
2024-05-04 21:26:26 -0500

    Se agrego la base de dato

---
+++ DLL/ConnectionManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL
+{
+    public class ConnectionManager
+    {
+        public string ConnectionString;
+        internal SqlConnection connection;
+        public ConnectionManager(string connetionString)
+        {
+            ConnectionString = connetionString;
+            connection = new SqlConnection(ConnectionString);
+        }
+        public void Open()
+        {
+            connection.Open();
+        }
+        public void Close()
+        {
+            connection.Close();
+        }
+    }
+}
