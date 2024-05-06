905f72d20b6b27411276151b1aa9826e242c5020
yeimmiscamacho <ylcamacho@unicesar.edu.co>
2024-05-05 21:42:54 -0500

    Se agrego

--- DLL/ClienteRepository.cs
+++ DLL/ClienteRepository.cs
@@ -1,28 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ENTITY;
 
 namespace DLL
 {
-public class ClienteRepository
+    public class ClienteRepository
     {
         SqlConnection connection;
-        List<ClsCliente> clientes;
-        public ClienteRepository(ConnectionManager Connection)
-        {
-            connection = Connection.connection;
-            clientes = new List<ClsCliente>();
-        }
-        public void GuardarCliente(ClsCliente cliente)
-        {
-            using(var comando = connection.CreateCommand() )
+      
 
-            {
-               
-            }
-        }
+      
     }
 }
