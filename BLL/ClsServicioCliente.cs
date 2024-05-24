6c80daec321a8869332325ae4ff86f5400a1f4b0
yeimmiscamacho <ylcamacho@unicesar.edu.co>
2024-05-24 18:44:25 -0500

    se agrego la cdena de conexion

--- BLL/ClsServicioCliente.cs
+++ BLL/ClsServicioCliente.cs
@@ -1,4 +1,5 @@
-﻿using ENTITY;
+﻿using DLL;
+using ENTITY;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -10,20 +11,54 @@ namespace BLL
 {
     public class ClsServicioCliente
     {
-        public ClsServicioCliente() 
-        { 
+        //public ClsServicioCliente() 
+        //{ 
+
+        //}
+        //public static string RegistroCliente (string tipoDocumento, string numeroDocumento, string nombre, string apellido, string nombreUsuario, string contraseña)
+        //{
+        //    ClsCliente cliente = new ClsCliente (tipoDocumento,numeroDocumento,nombre,apellido,nombreUsuario,contraseña);
+
+        //}
+        //public static DataTable RegistroCliente ( ClsCliente cliente)
+        //{
+        //    DataTable datosClientes = new DataTable();
+
+        //}
+
+
+        private ConexionDbDCostaFood conexion;
+            
+        private ClienteRepository clienteRepository;
+        List<ClsCliente> clientes;
+        ClsCliente cliente;
+
+        public ClsServicioCliente(string connectionString)
+        {
+            conexion = new ConexionDbDCostaFood(connectionString);
+
+            clienteRepository = new ClienteRepository(conexion);
+        }
+
+        public string GuardarCliente(ClsCliente cliente)
+        {
+            try
+            {
+                conexion.Open();
+
+                clienteRepository.GuardarCliente(cliente);
+
+                return $"Los datos del cliente  {cliente.Nombre} han sido guardados satiafactoriamente";
+            }
+            catch (Exception e)
+            {
+                return $"Error de base de datos: {e.Message}";
+            }
+            finally
+            {
+                conexion.Close();
+            }
 
         }
-        public static string RegistroCliente (string tipoDocumento, string numeroDocumento, string nombre, string apellido, string nombreUsuario, string contraseña)
-        {
-            ClsCliente cliente = new ClsCliente (tipoDocumento,numeroDocumento,nombre,apellido,nombreUsuario,contraseña);
-            
-        }
-        public static DataTable RegistroCliente ( ClsCliente cliente)
-        {
-            DataTable datosClientes = new DataTable();
-            
-        }
-
     }
 }
